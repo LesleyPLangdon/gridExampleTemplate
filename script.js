344ed36158a7e6849d313575372366247dcf4165
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-12-04 16:44:13 +0000

    function addToOrder

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+function addToOrder(item) {
+    const orderDisplay = document.getElementById('orderDisplay');
+    const listItem = document.createElement("p");
+    listItem.textContent = item;
+    orderDisplay.appendChild(listItem);
+}
