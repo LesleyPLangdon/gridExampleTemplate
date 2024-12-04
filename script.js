2b2e843f9db43029caa9d9a695bbf3120e81bb69
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-12-04 15:45:58 +0000

    finished

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
+let order = "";
+let finalOrder = "";
 
 function addToOrder(item) {
     const orderDisplay = document.getElementById('orderDisplay');
+    order += item;
+    orderDisplay.innerHTML = order
 
-    const listItem = document.getElementById('orderSpace');
-    listItem.textContent = item;
-    orderDisplay.appendChild(listItem);
+    // const listItem = document.createElement("p");
+    // listItem.textContent = item;
+    // orderDisplay.appendChild(listItem);
 }
 function submitOrder() {
+     finalOrder += order;
+     order = "";
+    
+    document.getElementById('finalOrder').innerHTML = finalOrder;
+     
     const orderDisplay = document.getElementById('orderDisplay');
 
     orderDisplay.innerHTML = "Thank you for your order";
