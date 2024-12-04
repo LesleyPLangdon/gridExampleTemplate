345df4140e672fa4db25f6d6e940f635bcf477f6
Lesley Potts-Langdon <61936741+LesleyPLangdon@users.noreply.github.com>
2024-12-04 14:30:17 +0000

    Added js functions addToOrder and submitOrder

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+function addToOrder(item) {
+    const orderDisplay = document.getElementById('orderDisplay');
+
+    const listItem = document.getElementById('orderSpace');
+    listItem.textContent = item;
+    orderDisplay.appendChild(listItem);
+}
+function submitOrder() {
+    const orderDisplay = document.getElementById('orderDisplay');
+
+    orderDisplay.innerHTML = "Thank you for your order";
+
+}
